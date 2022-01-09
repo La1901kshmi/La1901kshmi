3494aad993c402081905bb2e4ba1d9f0ed1eb38f
La1901kshmi <lucky98@23>
2022-01-09 17:07:14 +0530

    iterative

---
+++ palindrome checker.js
@@ -0,0 +1,13 @@
+
+function palindrome(str){
+    let len = str.length
+    str=str.toLowerCase()
+    for(let i=0;i<len/2;i++){
+    if(str[i]!==str[len-1-i])
+    {
+       return false
+    }
+}
+    return true
+}
+console.log(palindrome('lakshmi')
